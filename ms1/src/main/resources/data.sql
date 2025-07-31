35166d62786ebfa9f4b04b5d650dc6744cf3a3d4
pkozimor <kozimor@interia.eu>
2025-07-31 19:39:00 +0200

    add initial data.sql and request.http for content management

---
+++ ms1/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO contents(title, tag) VALUES ('TestSpring.pl', 'spring');
+INSERT INTO contents(title, tag) VALUES ('Architekt.pl', 'cloud');
+INSERT INTO contents(title, tag) VALUES ('Docker i Spring Cloud', 'cloud');
+INSERT INTO contents(title, tag) VALUES ('Docker Compose', 'docker');
